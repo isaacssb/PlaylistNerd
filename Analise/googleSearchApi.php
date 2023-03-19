6ced6f3f0694ea3a9084983b3c366b45a3b9266c
Isaac Barreto <isaacssb123@gmail.com>
2023-03-19 19:58:05 -0300

    feat: new method for generate excel in googleSearchApi

--- Analise/googleSearchApi.php
+++ Analise/googleSearchApi.php
@@ -55,14 +55,36 @@ class googleSearchApi
             throw new Exception('Erro ao realizar requisição para buscar os dados da API');
         }
 
-          echo '<div style="width: 90%; margin: 5px auto; padding: 5px; border: 1px solid #999; border-radius: 5px; background-color: #ccffff;">';
-          echo '<pre style="text-align: left;">';
-          echo   '<hr>';
-          echo   str_replace( $_SERVER['DOCUMENT_ROOT'], '', __FILE__) . ' (Linha ' . __LINE__ . ')';
-          echo   '<hr>';
-          var_dump( $response['items'] );
-          echo   '<hr>';
-          echo '</pre>';
-          echo '</div>';
+        // pegar posição  'displayLink', 'displayLink'
+        foreach ($response['items'] as $key => $info) {
+            echo '<div style="width: 90%; margin: 5px auto; padding: 5px; border: 1px solid #999; border-radius: 5px; background-color: #ccffff;">';
+            echo '<pre style="text-align: left;">';
+            echo   '<hr>';
+            echo   str_replace( $_SERVER['DOCUMENT_ROOT'], '', __FILE__) . ' (Linha ' . __LINE__ . ')';
+            echo   '<hr>';
+            var_dump( $info );
+            echo   '<hr>';
+            echo '</pre>';
+            echo '</div>';
+        }
+    }
+
+
+    public function generateExcel(array $columns, array $lines) 
+    {
+
+        if (count($columns) !== count($lines[0])) {
+            throw new Exception('Quantidade de colunas não condiz com a quantidade de linhas');
+        }
+        
+        $file = fopen('report.csv', 'w+');
+
+        fputcsv($file, $columns, ';');
+
+        foreach ($lines as $line) {
+            fputcsv($file, $line, ';');            
+        }
+
+        fclose($file);
     }
 }
--- Analise/index.php
+++ Analise/index.php
@@ -3,4 +3,10 @@ require_once('googleSearchApi.php');
 
 $googleSearchApi = new googleSearchApi();
 
-$googleSearchApi->getLinksInSearch('music list fullmetal alchemist');
+// $googleSearchApi->getLinksInSearch('music list fullmetal alchemist');
+
+
+$columns = array('Anime','Site','Link');
+$liks = array(array('FullMetal', 'fma.fandom.com', 'https://fma.fandom.com/wiki/Category:Music'));
+
+$googleSearchApi->generateExcel($columns, $liks);
